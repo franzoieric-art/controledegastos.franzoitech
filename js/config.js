8d0dac088627daa00745e99adcd14ae17570f76a
franzoieric-art <franzoieric@gmail.com>
2025-08-29 09:31:08 -0300

    config.js

--- js/config.js
+++ js/config.js
@@ -8,12 +8,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.15.0/firebase
 // autentique-se através de um backend (ex: Firebase Cloud Functions).
 
 const firebaseConfig = {
-    apiKey: "SUA_API_KEY_AQUI", // Substitua com seus valores
-    authDomain: "SEU_AUTH_DOMAIN_AQUI",
-    projectId: "SEU_PROJECT_ID_AQUI",
-    storageBucket: "SEU_STORAGE_BUCKET_AQUI",
-    messagingSenderId: "SEU_SENDER_ID_AQUI",
-    appId: "SEU_APP_ID_AQUI"
+    apiKey: "AIzaSyApSIolaeRxXdVNsnZkAvz_F_ILmxs1_K0",
+    authDomain: "AIzaSyApSIolaeRxXdVNsnZkAvz_F_ILmxs1_K0",
+    projectId: "controle-de-gastos-74daf",
+    storageBucket: "controle-de-gastos-74daf.firebasestorage.app",
+    messagingSenderId: "331667008487",
+    appId: "1:331667008487:web:c5200f64ac27d0c32feb42",
 };
 
 // Inicializa o Firebase e exporta as instâncias
