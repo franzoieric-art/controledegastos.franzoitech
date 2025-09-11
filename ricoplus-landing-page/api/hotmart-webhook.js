6129f8a3d6d470f367dc34132c5a8f0e4c6dcafc
franzoieric-art <franzoieric@gmail.com>
2025-09-11 03:54:32 -0300

    0354

---
+++ ricoplus-landing-page/api/hotmart-webhook.js
@@ -0,0 +1,8 @@
+// hotmart-webhook.js
+export default function handler(request, response) {
+  response.status(200).json({
+    body: request.body,
+    query: request.query,
+    cookies: request.cookies,
+  });
+}
