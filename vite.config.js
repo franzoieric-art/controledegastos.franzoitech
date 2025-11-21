fb7d35f4bbc9b333a0c7cde1692180e662c27e58
franzoieric-art <franzoieric@gmail.com>
2025-11-20 22:30:23 -0300

    2230

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,11 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        app: resolve(__dirname, 'app.html'),
-        landing: resolve(__dirname, 'landing.html')
+        // O PULO DO GATO: Chamamos de 'index' aqui para o Vite criar um 'index.html' na saída
+        index: resolve(__dirname, 'landing.html'), 
+        
+        // O App continua como app
+        app: resolve(__dirname, 'app.html')
       }
     }
   }
